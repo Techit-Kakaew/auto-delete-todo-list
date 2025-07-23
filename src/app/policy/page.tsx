c31ecb6a8e566bdbb1d6a13b8eb2ce9e34d35916
Techit Kakaew <techitkakaew@MacBook-Pro-6.local>
2025-07-23 20:38:41 +0700

    add facebook app id

---
+++ src/app/policy/page.tsx
@@ -0,0 +1,10 @@
+const PolicyPage = () => {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>This is the privacy policy for our website.</p>
+    </div>
+  );
+};
+
+export default PolicyPage;
---
+++ src/app/terms/page.tsx
@@ -0,0 +1,10 @@
+const TermsPage = () => {
+  return (
+    <div>
+      <h1>Terms of Service</h1>
+      <p>This is the terms of service for our website.</p>
+    </div>
+  );
+};
+
+export default TermsPage;
